56158c7a26a7a65aec5d7a123beda26f1fdbc76d
kareemezzat <kareemezzat900@gmail.com>
2025-06-28 18:59:45 +0300

    BestSellerListView

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,21 +1,27 @@
+import 'package:bookly_app/features/home/presentation/widgets/best_seller_itemlist.dart';
 import 'package:bookly_app/features/home/presentation/widgets/carousel_slider.dart';
 import 'package:bookly_app/features/home/presentation/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-  HomeView({super.key});
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CarouselSliderBooks(),
-          SizedBox(height: 20),
-          Text("Best Seller", style: Theme.of(context).textTheme.titleLarge),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CarouselSliderBooks(),
+            SizedBox(height: 20),
+            Text("Best Seller", style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 12),
+            Expanded(child: BestSellerItemListView()),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/home/presentation/widgets/best_seller_itemlist.dart
@@ -0,0 +1,19 @@
+import 'package:bookly_app/features/home/presentation/widgets/best_seller_item.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerItemListView extends StatelessWidget {
+  const BestSellerItemListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return BestSellerItem();
+      },
+      itemCount: 10,
+      separatorBuilder: (context, index) {
+        return SizedBox(height: 8);
+      },
+    );
+  }
+}
